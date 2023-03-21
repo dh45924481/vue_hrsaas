94c4f941762a69dad70c24d53ccc4cc837426425
IAN <45924481@qq.com>
2023-03-22 05:15:01 +0800

    更新

--- src/router/index.js
+++ src/router/index.js
@@ -178,7 +178,7 @@ const router = createRouter()
 // Detail see: https://github.com/vuejs/vue-router/issues/1234#issuecomment-357941465
 export function resetRouter() {
   const newRouter = createRouter()
-  router.matcher = newRouter.matcher // reset router
+  router.matcher = newRouter.matcher // reset  router
 }
 
 export default router
